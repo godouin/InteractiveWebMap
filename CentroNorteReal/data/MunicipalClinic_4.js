bd18e208e3df37d44748eb8d724fb561b44ce4fe
godouin <87271785+godouin@users.noreply.github.com>
2021-07-17 03:14:45 +0800

    Add files via upload

---
+++ CentroNorteReal/data/MunicipalClinic_4.js
@@ -0,0 +1,8 @@
+var json_MunicipalClinic_4 = {
+"type": "FeatureCollection",
+"name": "MunicipalClinic_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 121.569600344861016, 17.883508638358425 ] } }
+]
+}
