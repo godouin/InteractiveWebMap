9d98866e5479a7638d58897221b076de63f9c0b2
godouin <87271785+godouin@users.noreply.github.com>
2021-07-25 18:15:10 +0800

    Add files via upload

---
+++ TabangReal/data/BarangayHealthUnit_3.js
@@ -0,0 +1,8 @@
+var json_BarangayHealthUnit_3 = {
+"type": "FeatureCollection",
+"name": "BarangayHealthUnit_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 121.55138967038468, 17.817647793330785 ] } }
+]
+}
