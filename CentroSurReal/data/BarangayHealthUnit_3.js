558c00f68ab69bfa6658bccbbf983ef25df70030
godouin <87271785+godouin@users.noreply.github.com>
2021-07-25 18:13:18 +0800

    Delete CentroSurReal directory

--- CentroSurReal/data/BarangayHealthUnit_3.js
+++
@@ -1,8 +0,0 @@
-var json_BarangayHealthUnit_3 = {
-"type": "FeatureCollection",
-"name": "BarangayHealthUnit_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 121.569938151537102, 17.881136819373083 ] } }
-]
-}
