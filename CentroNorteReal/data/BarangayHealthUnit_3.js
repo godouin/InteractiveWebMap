01830c0d14b1d7afef56a8818f1729fe50d8bef4
godouin <87271785+godouin@users.noreply.github.com>
2021-07-25 18:13:27 +0800

    Delete CentroNorteReal directory

--- CentroNorteReal/data/BarangayHealthUnit_3.js
+++
@@ -1,8 +0,0 @@
-var json_BarangayHealthUnit_3 = {
-"type": "FeatureCollection",
-"name": "BarangayHealthUnit_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 121.571361859460083, 17.888458889384754 ] } }
-]
-}
